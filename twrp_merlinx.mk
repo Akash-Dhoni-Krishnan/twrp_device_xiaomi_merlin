4abdfb8cdef37e20c0fd7eb9c93f9e4dd8ee7621
Akash-Dhoni-Krishnan <113037849+Akash-Dhoni-Krishnan@users.noreply.github.com>
2023-04-21 11:51:33 +0530

    Update twrp_merlinx.mk

--- twrp_merlinx.mk
+++ twrp_merlinx.mk
@@ -20,8 +20,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from merlin device
-$(call inherit-product, device/xiaomi/merlin/device.mk)
+# Inherit from merlinx device
+$(call inherit-product, device/xiaomi/merlinx/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
