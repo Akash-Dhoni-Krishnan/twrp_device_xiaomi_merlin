bda056b872165b5f4520bc3d096dda0f914745cf
Akash-Dhoni-Krishnan <113037849+Akash-Dhoni-Krishnan@users.noreply.github.com>
2023-04-21 11:31:31 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,8 +17,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_merlin.mk
+    $(LOCAL_DIR)/twrp_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_merlin-userdebug \
-    twrp_merlin-eng
+    twrp_merlinx-userdebug \
+    twrp_merlinx-eng
